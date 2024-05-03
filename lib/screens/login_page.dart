3e25959d7da3b8a27dd8ba91542c82cbf752bf87
Hazrul <2021816444@student.uitm.edu.my>
2024-05-03 16:46:05 +0800

    validate login and register

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -3,95 +3,122 @@ import 'package:iconsax/iconsax.dart';
 import 'home_page.dart';
 
 class LoginPage extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Column(
-              children: [
-                Text(
-                  'MalayFood',
-                  style: TextStyle(fontSize: 32.0, color: Colors.black),
-                ),
-                SizedBox(height: 5.0), // Add margin between widgets
-                Text(
-                  'CalorieTracker',
-                  style: TextStyle(fontSize: 32.0, color: Colors.green),
-                ),
-                const SizedBox(height: 100.0),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius:
-                        BorderRadius.circular(20.0), // Set border radius here
-                    border: Border.all(
-                        color: Colors.grey), // Optional: Add border color
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Column(
+                children: [
+                  Text(
+                    'MalayFood',
+                    style: TextStyle(fontSize: 32.0, color: Colors.black),
                   ),
-                  child: SizedBox(
-                    width: 350,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: InputBorder.none, // Hide default border
-                        contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-                        prefixIcon: Icon(
-                          Icons.email,
-                          color: Colors.grey,
-                        ),
-                      ),
+                  SizedBox(height: 5.0), // Add margin between widgets
+                  Text(
+                    'CalorieTracker',
+                    style: TextStyle(fontSize: 32.0, color: Colors.green),
+                  ),
+                  const SizedBox(height: 100.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.circular(20.0), // Set border radius here
+                      border: Border.all(
+                          color: Colors.grey), // Optional: Add border color
                     ),
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius:
-                        BorderRadius.circular(20.0), // Set border radius here
-                    border: Border.all(
-                        color: Colors.grey), // Optional: Add border color
-                  ),
-                  child: SizedBox(
-                    width: 350,
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          labelText: 'Password',
+                    child: SizedBox(
+                      width: 350,
+                      child: TextFormField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
                           border: InputBorder.none, // Hide default border
                           contentPadding:
                               EdgeInsets.symmetric(horizontal: 16.0),
-                          prefixIcon: Icon(Icons.key)),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16.0),
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Add navigation logic here
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomePage(),
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Colors.grey,
+                          ),
                         ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            20.0), // Set button border radius here
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          // You can add more email validation logic here
+                          return null;
+                        },
                       ),
                     ),
-                    child: Text('Login'),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(height: 16.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.circular(20.0), // Set border radius here
+                      border: Border.all(
+                          color: Colors.grey), // Optional: Add border color
+                    ),
+                    child: SizedBox(
+                      width: 350,
+                      child: TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: InputBorder.none, // Hide default border
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 16.0),
+                            prefixIcon: Icon(Icons.key)),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          // You can add more password validation logic here
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // Add login logic here
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomePage(),
+                            ),
+                          );
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0), // Set button border radius here
+                        ),
+                      ),
+                      child: Text('Login'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/registeration_page.dart
+++ lib/screens/registeration_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:malay_food_cal_tracker/screens/login_page.dart';
 
 class RegistrationPage extends StatefulWidget {
   @override
@@ -6,7 +7,7 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController _usernameController = TextEditingController();
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
@@ -24,188 +25,248 @@ class _RegistrationPageState extends State<RegistrationPage> {
     'Extra active'
   ];
 
+  void _register() {
+    if (_formKey.currentState!.validate()) {
+      // Process registration data here
+      // You can navigate to the next screen or perform calculations
+      // based on the collected data
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Under development !!!'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 120.0),
-            Column(children: [
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 120.0),
+              Column(children: [
+                Text(
+                  'MalayFood',
+                  style: TextStyle(fontSize: 32.0, color: Colors.black),
+                ),
+              ]),
+              SizedBox(height: 5.0), // Add margin between widgets
               Text(
-                'MalayFood',
-                style: TextStyle(fontSize: 32.0, color: Colors.black),
+                'CalorieTracker',
+                style: TextStyle(fontSize: 32.0, color: Colors.green),
               ),
-            ]),
-            SizedBox(height: 5.0), // Add margin between widgets
-            Text(
-              'CalorieTracker',
-              style: TextStyle(fontSize: 32.0, color: Colors.green),
-            ),
-            SizedBox(height: 60.0),
-            Row(
-              children: [
-                Expanded(
-                  //username input
-                  child: TextField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
+              SizedBox(height: 60.0),
+              Row(
+                children: [
+                  Expanded(
+                    //username input
+                    child: TextFormField(
+                      controller: _usernameController,
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a username';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-                SizedBox(width: 16.0),
-                Expanded(
-                  child: TextField(
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an email address';
+                        }
+                        // You can add more email validation logic here
+                        return null;
+                      },
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a password';
+                        }
+                        // You can add more password validation logic here
+                        return null;
+                      },
                     ),
                   ),
-                ),
-                SizedBox(width: 16.0),
-                Expanded(
-                  child: DropdownButtonFormField<String>(
-                    value: _selectedGender,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _selectedGender = newValue!;
-                      });
-                    },
-                    items: <String>['Male', 'Female'].map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    decoration: InputDecoration(
-                      labelText: 'Gender',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _weightController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Weight (kg)',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 16.0),
-                Expanded(
-                  child: TextField(
-                    controller: _heightController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Height (cm)',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _ageController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Age',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 16.0),
-                Expanded(
-                  child: DropdownButtonFormField<String>(
-                    isExpanded: true,
-                    value: _selectedActivityLevel,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _selectedActivityLevel = newValue!;
-                      });
-                    },
-                    items: _activityLevels.map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    decoration: InputDecoration(
-                      labelText: 'Activity Level',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: DropdownButtonFormField<String>(
+                      value: _selectedGender,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedGender = newValue!;
+                        });
+                      },
+                      items: <String>['Male', 'Female'].map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      decoration: InputDecoration(
+                        labelText: 'Gender',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
                       ),
                     ),
                   ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _weightController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Weight (kg)',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your weight';
+                        }
+                        // You can add more weight validation logic here
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _heightController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Height (cm)',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your height';
+                        }
+                        // You can add more height validation logic here
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _ageController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Age',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your age';
+                        }
+                        // You can add more age validation logic here
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: DropdownButtonFormField<String>(
+                      isExpanded: true,
+                      value: _selectedActivityLevel,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedActivityLevel = newValue!;
+                        });
+                      },
+                      items: _activityLevels.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      decoration: InputDecoration(
+                        labelText: 'Activity Level',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              SizedBox(
+                width: 250,
+                child: ElevatedButton(
+                  onPressed: _register,
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Register',
+                    style: TextStyle(fontSize: 15),
+                  ),
                 ),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            SizedBox(
-              width: 250,
-              child: ElevatedButton(
+              ),
+              SizedBox(height: 10.0), // Add space below the register button
+              TextButton(
                 onPressed: () {
-                  // Process registration data here
-                  // You can navigate to the next screen or perform calculations
-                  // based on the collected data
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Under development !!!'),
-                      duration: Duration(seconds: 2),
+                  // Navigate to login page or perform any other action
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(),
                     ),
                   );
                 },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                        20.0), // Set button border radius here
-                  ),
-                ),
-                child: Text(
-                  'Register',
-                  style: TextStyle(fontSize: 15),
-                ),
+                child: Text('Already has an account? Login'),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
